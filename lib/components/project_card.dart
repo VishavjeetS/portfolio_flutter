52f87a5d19490cb85abf35e51d1e6df51bfa4608
VishavjeetS <vishavhanspal00@gmail.com>
2023-05-30 20:35:38 +0530

    Assets-github

--- lib/components/project_card.dart
+++ lib/components/project_card.dart
@@ -84,7 +84,7 @@ class _ProjectCardState extends State<ProjectCard> {
                           Padding(
                               padding: const EdgeInsets.all(8.0),
                               child: Image.network(
-                                "https://github.com/VishavjeetS/portfolio_flutter/blob/flutter_app/assets/images/github.png",
+                                "assets/images/github.png",
                                 fit: BoxFit.cover,
                                 height: 25,
                                 width: 25,
